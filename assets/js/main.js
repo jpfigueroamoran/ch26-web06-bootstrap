4b919eb91eaa7c1f43c2cdf8956ebb9028613e47
jp.figueroa <jp.figueroamoran@gmail.com>
2023-04-22 13:41:30 -0600

    Se desarollo un portfolio de ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+console.log("Test de conexión");
+
+function changeColorToGreen(){
+    const refName = document.getElementById("name");
+
+    refName.style.color = "green";
+}
+
+function changeColorToBlack(){
+    const refName = document.getElementById("name");
+
+    refName.style.color = "black";
+}
+function changeColorToWhite(){
+    const refName = document.getElementById("name");
+
+    refName.style.color = "white";
+}
