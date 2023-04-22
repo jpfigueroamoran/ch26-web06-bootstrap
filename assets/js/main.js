efecced55473a5b6a28cafff3a17f14944be6117
jp.figueroa <jp.figueroamoran@gmail.com>
2023-04-22 15:05:13 -0600

    Feat: Se agrego la app -Di mi nombre

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,16 @@ function changeColorToWhite(){
 
     refName.style.color = "white";
 }
+
+function cambiarNombre() {
+    var nombre = prompt("¿Cuál es tu nombre?");
+  
+    if (nombre != null) {
+      var mensaje = document.getElementById("mensaje");
+      mensaje.innerHTML = "Hola " + nombre;
+    }
+    else {
+      var mensaje = document.getElementById("mensaje");
+      mensaje.innerHTML = "Por favor ingrese un nombre";
+    }
+  }
